43ddc7156c319282562b0ca980838e8fe973e8b0
EichZz <hectoramm.rubio@gmail.com>
2020-05-01 21:00:01 +0200

    Merge origin/master
    Conflicts:
    	Aggregator/src/java/struts.xml

---
+++ Aggregator/src/java/acciones/NewNoticia.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acciones;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.Date;
+import java.util.Map;
+import utils.Utils;
+
+/**
+ *
+ * @author hecto
+ */
+public class NewNoticia extends ActionSupport {
+    
+    private String titulo;
+    private String descripcion;
+    private String fuente;
+    
+    public NewNoticia() {
+    }
+    
+    public String execute() throws Exception {
+       Map session = (Map) ActionContext.getContext().get("session");
+       Date fechaPublicacion = Utils.fechaHoy();
+       
+       
+         return "JUAN CALLATE";
+    }
+    
+}
