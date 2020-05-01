d201a02eac0266048a83418abe77d9a9a8fa06be
YellowZaki <algoca99@gmail.com>
2020-05-02 00:01:40 +0200

    Agregado SetVariables, una acción que contendrá todas aquellas variables que se deben setear antes de mostrar un jsp

--- Aggregator/src/java/dao/noticiaDAO.java
+++ Aggregator/src/java/dao/noticiaDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -21,4 +22,15 @@ public class noticiaDAO {
         tx.commit();
         return n;
     }
+    
+    public List<Noticia> getAllNoticias() {
+        sesion = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = sesion.beginTransaction();
+        Query q = sesion.createQuery("From Noticia");
+        List<Noticia> ln = (List<Noticia>) q.list();
+        tx.commit();
+        return ln;
+    } 
+    
+    
 }
