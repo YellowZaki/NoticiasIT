c1b3a1b69f01c0554bade40e825166cd609690e7
Andres <7bamcm@gmail.com>
2020-05-19 18:40:20 +0200

    Imagen tema

--- Aggregator/src/java/acciones/NoticiaActions.java
+++ Aggregator/src/java/acciones/NoticiaActions.java
@@ -26,7 +26,6 @@ public class NoticiaActions extends ActionSupport {
     private String descripcion;
     private String fuente;
     private String temaNoticia;
-    private String actualizar;
     private String id;
 
     public String getId() {
@@ -35,14 +34,6 @@ public class NoticiaActions extends ActionSupport {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getActualizar() {
-        return actualizar;
-    }
-
-    public void setActualizar(String actualizar) {
-        this.actualizar = actualizar;
     }
 
     public String getTitulo() {
@@ -89,8 +80,14 @@ public class NoticiaActions extends ActionSupport {
         temaDAO tdao = new temaDAO();
         Tema tema = tdao.getTema(temaNoticia);
 
-        if (this.getId() != null) {//actualizar
-            
+        if (getId() != null) {//actualizar
+            Noticia noticia = ndao.getNoticia(getId());
+
+            noticia.setTema(tema);
+            noticia.setTitulo(titulo);
+            noticia.setDescripcion(descripcion);
+            noticia.setFuente(fuente);
+            ndao.updateNoticia(noticia);
         } else {
             Noticia noticia = new Noticia(tema, usuario, titulo, descripcion, fuente, fechaPublicacion);
             ndao.addNoticia(noticia);
