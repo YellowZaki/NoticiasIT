108ffaa1e33ad0c6e8c1b78559bc140a558d601c
EichZz <hectoramm.rubio@gmail.com>
2020-05-19 16:44:32 +0200

    editar noticia a medias

--- Aggregator/src/java/acciones/NoticiaActions.java
+++ Aggregator/src/java/acciones/NoticiaActions.java
@@ -21,11 +21,29 @@ import utils.Utils;
  * @author hecto
  */
 public class NoticiaActions extends ActionSupport {
-    
+
     private String titulo;
     private String descripcion;
     private String fuente;
     private String temaNoticia;
+    private String actualizar;
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getActualizar() {
+        return actualizar;
+    }
+
+    public void setActualizar(String actualizar) {
+        this.actualizar = actualizar;
+    }
 
     public String getTitulo() {
         return titulo;
@@ -58,24 +76,27 @@ public class NoticiaActions extends ActionSupport {
     public void setTemaNoticia(String temaNoticia) {
         this.temaNoticia = temaNoticia;
     }
-    
+
     public NoticiaActions() {
     }
-    
+
     public String execute() throws Exception {
-       Map session = (Map) ActionContext.getContext().get("session");
-       Usuario usuario = (Usuario) session.get("usuario");
-       
-       Date fechaPublicacion = Utils.fechaHoy();
+        Map session = (Map) ActionContext.getContext().get("session");
+        Usuario usuario = (Usuario) session.get("usuario");
+        noticiaDAO ndao = new noticiaDAO();
+
+        Date fechaPublicacion = Utils.fechaHoy();
         temaDAO tdao = new temaDAO();
-       Tema tema = tdao.getTema(temaNoticia);
-       
-       Noticia noticia = new Noticia(tema, usuario, titulo, descripcion, fuente, fechaPublicacion);
-       noticiaDAO ndao = new noticiaDAO();
-       
-       ndao.addNoticia(noticia);
-       
+        Tema tema = tdao.getTema(temaNoticia);
+
+        if (this.getId() != null) {//actualizar
+            
+        } else {
+            Noticia noticia = new Noticia(tema, usuario, titulo, descripcion, fuente, fechaPublicacion);
+            ndao.addNoticia(noticia);
+        }
+
         return SUCCESS;
     }
-    
+
 }
